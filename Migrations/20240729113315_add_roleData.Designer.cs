7cec024eb3be2cc2f510f8db8c4ca1a43ebf3355
Iris Shing <irisshing1211@gmail.com>
2024-07-30 16:40:59 +0800

    add default role: admin, Hmpy

--- Migrations/20240729113315_add_roleData.Designer.cs
+++ Migrations/20240729113315_add_roleData.Designer.cs
@@ -4,6 +4,7 @@ using AuthSample.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AuthSample.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240729113315_add_roleData")]
+    partial class add_roleData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Configurations/RoleConfiguration.cs
+++ Models/Configurations/RoleConfiguration.cs
@@ -9,11 +9,23 @@ public class RoleConfiguration : IEntityTypeConfiguration<Role>
     {
         // 需在中間表 RolePermissions 設定 ForeignKey, 可實現直接取得 role.permissions[]
         // 只需設一個，另一邊 ef 會自動產
-        builder.HasMany(a => a.Permissions).WithMany(r => r.Roles).UsingEntity<RolePermission>( 
-            // 要注意 join 順序
-            j=>j.HasOne<Permission>().WithMany(r=>r.RolePermissions).HasForeignKey(k=>k.PermissionId),
-            j=>j.HasOne<Role>().WithMany(p=>p.RolePermissions).HasForeignKey(k=>k.RoleId),
-            j=>j.ToTable("RolePermissions"));
+        builder.HasMany(a => a.Permissions)
+               .WithMany(r => r.Roles)
+               .UsingEntity<RolePermission>(
 
+                   // 要注意 join 順序
+                   j => j.HasOne<Permission>().WithMany(r => r.RolePermissions).HasForeignKey(k => k.PermissionId),
+                   j => j.HasOne<Role>().WithMany(p => p.RolePermissions).HasForeignKey(k => k.RoleId),
+                   j => j.ToTable("RolePermissions"));
+
+        builder.HasData(
+            new Role()
+            {
+                Id = Guid.Parse("0a9e4508-0078-44f7-ae1e-ce07d65a84be"), Name = "Admin", NormalizedName = "Admin"
+            },
+            new Role()
+            {
+                Id = Guid.Parse("93779de5-302c-4dfc-ac5f-b3dccc155f3d"), Name = "Hmpy", NormalizedName = "基本權限"
+            });
     }
 }
